ad011ece16c9f2ddf690813160ab322ce829d7dd
Mamun <mamunkhan2002bd@gmail.com>
2024-02-16 19:54:27 +0600

    çompleted incrase section

--- script/script.js
+++ script/script.js
@@ -7,8 +7,33 @@
 
 // }
 function playerPress(event) {
-    const alphabet = event.key;
-    return alphabet;
+    // get the player press alphabet key 
+    const pressAlphabet = event.key;
+    // console.log('player press alphabet', pressAlphabet);
+
+    // get the current Alphabet key 
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log('Expected alphabet', expectedAlphabet);
+    // console.log(expectedAlphabet, pressAlphabet);
+    // check right or not 
+    if (pressAlphabet === expectedAlphabet) {
+        console.log("You got a point");
+        const currentScoreElement = document.getElementById("current-score");
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        const score = currentScore + 1;
+        currentScoreElement.innerText = score;
+
+        console.log(score);
+        removeColorElementById(expectedAlphabet)
+        continueGame();
+
+    } else {
+        console.log("you loss a life");
+    }
+
 }
 
 document.addEventListener('keyup', playerPress)
@@ -17,15 +42,9 @@ function continueGame() {
     const alphabet = getARandomAlphabet();
     // display the current alphabet 
     const currentAlphabetElement = document.getElementById('current-alphabet');
-    const currentAlphabet = currentAlphabetElement.innerText = alphabet;
-    const currentKeyAlphabet = setColorElementById(currentAlphabet);
-    const playerPress = playerPress();
-
-    if (alphabet === playerPress) {
-        console.log(alphabet, playerPress);
-        continueGame();
-    }
-
+    currentAlphabetElement.innerText = alphabet
+    // // set press key color 
+    setColorElementById(alphabet)
 }
 
 function playButton() {
--- script/utility.js
+++ script/utility.js
@@ -9,20 +9,25 @@ function showElementById(elementId) {
 }
 
 function setColorElementById(elementId) {
-    const element = document.getElementById(elementId)
+    const element = document.getElementById(elementId);
     element.style.backgroundColor = "#FFA500";
+}
+
+function removeColorElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.style.backgroundColor = "";
 }
 
 function getARandomAlphabet() {
     const alphabetString = 'abcdefghijkmnopqrstuvwxyz';
     const alphabets = alphabetString.split('');
-    console.log(alphabets);
+    // console.log(alphabets);
 
     // get the random index  number 
-    const randomNumber = Math.random() * 25;
+    const randomNumber = Math.random() * 24;
     const index = Math.round(randomNumber);
     const alphabet = alphabets[index];
-    console.log(index, alphabet);
+    // console.log(index, alphabet);
     return alphabet;
 
 }
